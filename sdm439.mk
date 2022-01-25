dc2a2b2ed23f40ad1ef8bec2670e34ef4b3b6db4
skittels15 <skittels1510@mail.ru>
2022-01-25 23:38:25 +0300

    sdm439-common: All overlays moved to device

--- sdm439.mk
+++ sdm439.mk
@@ -8,9 +8,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, device/xiaomi/sdm439-common/device-hidl.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
